d905e27be9f0e7288ae1466d44bd151465eb0e21
hafidzrizqullahprasetya <hafidzrizqullahprasetya@users.noreply.github.com>
2026-08-29 00:50:29 +0700

    feat: convert github-regkit-private to git submodule and remove github-regkit-mibp

--- github-regkit-private/github_register/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Automated GitHub sign-up toolkit (Camoufox + Litensi mail)."""
-
-from .config import Config, load_config
-from .runner import register_one, run_job
-
-__all__ = ["Config", "load_config", "register_one", "run_job"]
